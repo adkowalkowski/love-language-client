f192d24e2c088e9fb2d296cfb48b956f5889ac3c
anthony <adkowalkowski@gmail.com>
2022-03-29 09:55:41 -0500

    update styling on signup form

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Form from "react-bootstrap/Form"
 import Button from 'react-bootstrap/Button'
+import './SignUp.css'
 
 const SignUp = () => {
   return (
@@ -18,7 +19,7 @@ const SignUp = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
