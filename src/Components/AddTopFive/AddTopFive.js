8a59bf7fd63cc9940030a2ebb330eb346bc64eec
anthony <adkowalkowski@gmail.com>
2022-03-31 12:08:47 -0500

    Finish top5 post request

--- src/Components/AddTopFive/AddTopFive.js
+++ src/Components/AddTopFive/AddTopFive.js
@@ -1,102 +1,168 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import './AddTopFive.css'
-import { SignIn, user }from "../SignIn/SignIn";
+import "./AddTopFive.css";
+import { SignIn, user } from "../SignIn/SignIn";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
-
 const AddTopFive = () => {
+  // CONDITIONAL RENDERING COULD BE if (!token) render SignIn page if(token) render AddTopFive
+  const token = localStorage.token;
+  console.log(token);
 
-  // CONDITIONAL RENDERING COULD BE if (!token) render SignIn page if(token) render AddTopFive 
-  const token = localStorage.token
-  console.log(token)
+  //   const [topFive, setTopFive] = useState({
+  //       one: "",
+  //       two: "",
+  //       three: "",
+  //       four: "",
+  //       five: ""
+  //   })
 
-  const [topFive, setTopFive] = useState({
-      one: "",
-      two: "",
-      three: "",
-      four: "",
-      five: ""
-  })
-  
-//   const handleSelect = (e) => {
-//     setTopFive((prevTopFive) => {
+  // STATES FOR ADDING A USER'S TOP 5
+  const [one, setOne] = useState("");
+  const [two, setTwo] = useState("");
+  const [three, setThree] = useState("");
+  const [four, setFour] = useState("");
+  const [five, setFive] = useState("");
 
-//     }
-//   };
+  // STATE FOR RENDERING DATA
+  const [topFive, setTopFive] = useState([])
 
+  // HANDLESELECT FOR POSTING A USER'S TOP 5
+  const handleOneSelect = (e) => {
+    e.preventDefault();
+    e.persist();
+    const selectedValue = e.target.value;
+    setOne(selectedValue);
+  };
+
+  const handleTwoSelect = (e) => {
+    e.preventDefault();
+    e.persist();
+    const selectedValue = e.target.value;
+    setTwo(selectedValue);
+  };
+
+  const handleThreeSelect = (e) => {
+    e.preventDefault();
+    e.persist();
+    const selectedValue = e.target.value;
+    setThree(selectedValue);
+  };
+
+  const handleFourSelect = (e) => {
+    e.preventDefault();
+    e.persist();
+    const selectedValue = e.target.value;
+    setFour(selectedValue);
+  };
+
+  const handleFiveSelect = (e) => {
+    e.preventDefault();
+    e.persist();
+    const selectedValue = e.target.value;
+    setFive(selectedValue);
+  };
+
+  // POST REQUEST FOR ADDING TOP 5 TO A USER'S ACCOUNT
   const handleTopFiveSubmit = () => {
     const topFiveData = {
-      one: topFive.one,
-      two: topFive.two,
-      three: topFive.three,
-      four: topFive.four,
-      five: topFive.five
+      one: one,
+      two: two,
+      three: three,
+      four: four,
+      five: five,
     };
-     axios
+    axios
       .post("http://127.0.0.1:8000/love-languages/", topFiveData, {
-          headers: {
-            'Authorization': `Token ${token}` 
-          }
+        headers: {
+          Authorization: `Token ${token}`,
+        },
       })
       .then((response) => {
-        setTopFive(response.data);
+        setOne(response.data.one);
+        setTwo(response.data.two);
+        setThree(response.data.three);
+        setFour(response.data.four);
+        setFive(response.data.five);
+        console.log(response.data)
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  const handleGet = () => {
+      axios
+        .get("http://127.0.0.1:8000/love-languages/")
+        .then((res) => {
+
+        })
+  }
+
   return (
     <div className="top-five-form">
       <Form onSubmit={handleTopFiveSubmit}>
-      <Form.Select aria-label="Default select example">
-        <option>Number One</option>
-        <option value="1">Acts of Service</option>
-        <option value="2">Quality Time</option>
-        <option value="3">Words of Affirmation</option>
-        <option value="4">Receiving Gifts</option>
-        <option value="5">Physical Touch</option>
-      </Form.Select>
-      <Form.Select aria-label="Default select example">
-        <option>Number Two</option>
-        <option value="1">Acts of Service</option>
-        <option value="2">Quality Time</option>
-        <option value="3">Words of Affirmation</option>
-        <option value="4">Receiving Gifts</option>
-        <option value="5">Physical Touch</option>
-      </Form.Select>
-      <Form.Select aria-label="Default select example">
-        <option>Number Three</option>
-        <option value="1">Acts of Service</option>
-        <option value="2">Quality Time</option>
-        <option value="3">Words of Affirmation</option>
-        <option value="4">Receiving Gifts</option>
-        <option value="5">Physical Touch</option>
-      </Form.Select>
-      <Form.Select aria-label="Default select example">
-        <option>Number Four</option>
-        <option value="1">Acts of Service</option>
-        <option value="2">Quality Time</option>
-        <option value="3">Words of Affirmation</option>
-        <option value="4">Receiving Gifts</option>
-        <option value="5">Physical Touch</option>
-      </Form.Select>
-      <Form.Select aria-label="Default select example">
-        <option>Number Five</option>
-        <option value="1">Acts of Service</option>
-        <option value="2">Quality Time</option>
-        <option value="3">Words of Affirmation</option>
-        <option value="4">Receiving Gifts</option>
-        <option value="5">Physical Touch</option>
-      </Form.Select>
-      <Button variant="primary" type="submit">
-            Submit
-          </Button>
+        <Form.Select
+          onChange={handleOneSelect}
+          aria-label="Default select example"
+        >
+          <option>Number One</option>
+          <option>Acts of Service</option>
+          <option>Quality Time</option>
+          <option>Words of Affirmation</option>
+          <option>Receiving Gifts</option>
+          <option>Physical Touch</option>
+        </Form.Select>
+        <Form.Select
+          onChange={handleTwoSelect}
+          aria-label="Default select example"
+        >
+          <option>Number Two</option>
+          <option>Acts of Service</option>
+          <option>Quality Time</option>
+          <option>Words of Affirmation</option>
+          <option>Receiving Gifts</option>
+          <option>Physical Touch</option>
+        </Form.Select>
+        <Form.Select
+          onChange={handleThreeSelect}
+          aria-label="Default select example"
+        >
+          <option>Number Three</option>
+          <option>Acts of Service</option>
+          <option>Quality Time</option>
+          <option>Words of Affirmation</option>
+          <option>Receiving Gifts</option>
+          <option>Physical Touch</option>
+        </Form.Select>
+        <Form.Select
+          onChange={handleFourSelect}
+          aria-label="Default select example"
+        >
+          <option>Number Four</option>
+          <option>Acts of Service</option>
+          <option>Quality Time</option>
+          <option>Words of Affirmation</option>
+          <option>Receiving Gifts</option>
+          <option>Physical Touch</option>
+        </Form.Select>
+        <Form.Select
+          onChange={handleFiveSelect}
+          aria-label="Default select example"
+        >
+          <option>Number Five</option>
+          <option>Acts of Service</option>
+          <option>Quality Time</option>
+          <option>Words of Affirmation</option>
+          <option>Receiving Gifts</option>
+          <option>Physical Touch</option>
+        </Form.Select>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
-      
     </div>
   );
 };
